5f39cb2cfb61407b635b37d47e426c36adb42bc9
yangjc <bupt_yjc@163.com>
2019-01-07 20:22:44 +0800

    fix predict

--- recognize.py
+++ recognize.py
@@ -2,13 +2,10 @@ import pickle as pk
 
 import numpy as np
 
-from keras.models import load_model
+import torch
+import torch.nn.functional as F
 
-from keras.preprocessing.sequence import pad_sequences
-
-from represent import add_buf
-
-from nn_arch import cnn, rnn
+from represent import sent2ind, add_buf
 
 from util import map_item
 
@@ -20,37 +17,37 @@ def ind2label(label_inds):
     return ind_labels
 
 
+device = torch.device('cpu')
+
 seq_len = 50
 
-path_word2ind = 'model/word2ind.pkl'
-path_embed = 'feat/embed.pkl'
+path_word_ind = 'feat/word_ind.pkl'
 path_label_ind = 'feat/label_ind.pkl'
-with open(path_word2ind, 'rb') as f:
-    word2ind = pk.load(f)
-with open(path_embed, 'rb') as f:
-    embed_mat = pk.load(f)
+with open(path_word_ind, 'rb') as f:
+    word_inds = pk.load(f)
 with open(path_label_ind, 'rb') as f:
     label_inds = pk.load(f)
 
 ind_labels = ind2label(label_inds)
 
-funcs = {'cnn': cnn,
-         'rnn': rnn}
+paths = {'cnn': 'model/cnn.pkl',
+         'rnn': 'model/rnn.pkl'}
 
-paths = {'cnn': 'model/cnn.h5',
-         'rnn': 'model/rnn.h5'}
-
-models = {'cnn': load_model(map_item('cnn', paths)),
-          'rnn': load_model(map_item('rnn', paths))}
+models = {'cnn': torch.load(map_item('cnn', paths), map_location=device),
+          'rnn': torch.load(map_item('rnn', paths), map_location=device)}
 
 
 def predict(text, name):
-    seq = word2ind.texts_to_sequences([text])[0]
-    pad_seq = pad_sequences([seq], maxlen=seq_len)
+    text = text.strip()
+    pad_seq = sent2ind(text, word_inds, seq_len, keep_oov=True)
     if name == 'cnn':
-        pad_seq = add_buf(pad_seq)
+        pad_seq = add_buf([pad_seq])[0]
+    sent = torch.LongTensor([pad_seq]).to(device)
     model = map_item(name, models)
-    probs = model.predict(pad_seq)[0]
+    with torch.no_grad():
+        model.eval()
+        probs = F.softmax(model(sent), dim=1)
+    probs = probs.numpy()[0]
     inds = np.argmax(probs, axis=1)
     preds = [ind_labels[ind] for ind in inds[-len(text):]]
     pairs = list()
